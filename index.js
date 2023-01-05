5fbeeb623562422c44b7e1811b04f1dca1c6554b
8bitlogan <lokeener@student.ubc.ca>
2023-01-05 15:28:40 -0500

    successful

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // load in puppeteer
 const puppeteer = require('puppeteer')
 const { BrowserFetcher } = require('puppeteer-core')
+const fs = require('fs')
 
 let deptlinks = [];
 
@@ -12,6 +13,7 @@ let deptlinks = [];
  - each course has an associated list of availabilities
  - some courses can be split into 2+ sub-courses, like a lecture and a tutorial
      - idk how to handle this, will consider later
+     - need to also handle waiting lists
 
 how to represent this as a csv:
  - each row is a course section with its name, start, end, Prereqs, Coreqs, and type
@@ -23,15 +25,11 @@ void (async () => {
     
 
     try {
-        console.log('start')
-
         // new browser instance
         const browser = await puppeteer.launch({headless: false})
-        console.log('launched')
 
         // create a page in the browser
         const page = await browser.newPage()
-        console.log('done')
 
         // navigate
         await page.goto('https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-all-departments')
@@ -43,19 +41,15 @@ void (async () => {
             let items = document.querySelectorAll("#mainTable > tbody > tr");
             items.forEach ((item) => {
 
-
                 let i = $($(item).children().first()).children().first().attr('href')
                 if (i) {
                     deptlinks.push(i);
-
-                    
                 }
             })
 
             return deptlinks;
         });
 
-        console.log(deptdata);
 
         const newPage = await browser.newPage();
 
@@ -86,10 +80,7 @@ void (async () => {
             
         }
 
-        console.log(crs);
-
-
-        // get course section data
+        // get course section links
         
 
         const thirdPage = await browser.newPage();
@@ -102,7 +93,7 @@ void (async () => {
 
                 await thirdPage.goto(url);
 
-                const contents = await thirdPage.evaluate(() => {
+                let contents = await thirdPage.evaluate(() => {
                     let sectionlinks = [];
 
                     let sections = document.querySelectorAll("body > div.container > div.content.expand > table.table.table-striped.section-summary > tbody > tr");
@@ -114,19 +105,122 @@ void (async () => {
                             sectionlinks.push(s)
                         }
                     })
-                    
 
-                    return sectionlinks;
+                    return sectionlinks
                 })
 
                 sects.push(contents)
+
             }
             
         } 
 
         console.log(sects);
 
+        // get section data
+
+        let init = [{
+            dept: 'INIT',
+            course: 'INIT',
+            sect: 'INIT',
+            term: 'INIT',
+            day: 'INIT',
+            start: 'INIT',
+            end: 'INIT'
+        }]
+
+        const i = JSON.stringify(init)
+
+        fs.writeFile('./section.json', i, 'utf8', err => {
+            if (err) {
+                console.log('Error writing file')
+            } else {
+                console.log('File written successfully')
+            }
+        })
+
+        const fourthPage = await browser.newPage();
+
+
+        for (const section of sects) {
+            for (const link of section) {
+                let url = "https://courses.students.ubc.ca" + link;
+
+                await fourthPage.goto(url);
+
+                let trimmed = link.substring(58, link.length)
+                let dept = trimmed.substring(0, trimmed.indexOf('&'))
+
+                let trimmed1 = trimmed.substring(trimmed.indexOf('&') + 8, trimmed.length)
+                let course = trimmed1.substring(0, trimmed1.indexOf('&'))
+
+                let sect = trimmed1.substring(trimmed1.indexOf('=') + 1, trimmed1.length)
+
+                const [one, two, three, four]= await fourthPage.evaluate(() => {
+
+                    let body = $('.table.table.table-striped').children().first().next()
         
+                    var term = ' ';
+                    var day = '';
+                    var start = ' ';
+                    var end = ' ';
+                    
+                    $($(body).children().children()).each(function(i) {
+                        switch (i) {
+                            case 0:
+                                term = term + $(this).text();
+                                break;
+                            case 1:
+                                day = day + $(this).text();
+                                break;
+                            case 2:
+                                start = start + $(this).text();
+                                break;
+                            case 3:
+                                end = end + $(this).text();
+                                
+                        }
+                        
+                    })
+        
+                    return [
+                        term,
+                        day,
+                        start,
+                        end
+                    ];
+         
+                })
+        
+
+                fs.readFile('./section.json', 'utf8', (err, data) => {
+                    if (err) {
+                        console.log("error reading file")
+                    } else {
+
+                        const section = JSON.parse(data);
+
+                        section.push({
+                            dept: dept,
+                            course: course,
+                            sect: sect,
+                            term: one,
+                            day: two,
+                            start: three,
+                            end: four
+                        })
+
+                        fs.writeFile('./section.json', JSON.stringify(section, null, 7), err => {
+                            if (err) {
+                                console.log('error writing file')
+                            }
+                        })
+                    } 
+                    
+                })
+
+            }
+        }
         
         await browser.close()
         
